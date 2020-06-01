9a42844c50a2d225b627a13ddf20025d42bf14e5
AlexL70 <alexander.levinson.70@gmail.com>
2020-06-01 21:10:20 +0300

    27. Generating a Secure Json Web Token

---
+++ Starter/IdentityNetCore/Controllers/ProductsController.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections.Generic;
+using IdentityNetCore.Models;
+using Microsoft.AspNetCore.Mvc;
+
+namespace IdentityNetCore.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class ProductsController : ControllerBase
+    {
+        [Route(template: "List")]
+        public List<Product> GetList()
+        {
+            return new List<Product>
+            {
+                new Product { Name = "Chair", Price = 97.12m },
+                new Product { Name = "Desk", Price = 120.50m }
+            };
+        }
+    }
+}
