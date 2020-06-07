ff25e7742d82c81b1511cc38281f1696b6b402de
AlexL70 <alexander.levinson.70@gmail.com>
2020-06-07 21:00:16 +0300

    31. Cross Site Scripting Attack (XSS)

---
+++ SecurityRisks/XSS/StudentEnroll/StudentEnroll/Controllers/StudentController.cs
@@ -0,0 +1,20 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using StudentEnroll.Models;
+
+namespace StudentEnroll.Controllers
+{
+    public class StudentsController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View("Index");
+        }
+
+
+        [HttpPost]
+        public IActionResult SignUp(StudentViewModel model)
+        {
+            return View("Result", model);
+        }
+    }
+}
